51576762d578941d1a249ec1c379543b96b858d3
vikramrj30 <84263965+vikramrj30@users.noreply.github.com>
2021-05-16 13:22:32 +0530

    created food item
    this file  prints food items

---
+++ fooditem.c
@@ -0,0 +1,24 @@
+
+#include<studio.h>
+main()
+{
+   int choice=0;
+   printf("pick an item:");
+   scanf("%d,&Choice");
+   
+   switch(choice)
+   {
+      case 1;
+        printf("pick a pizza");
+      case 2;
+         printf("pick a burger");
+      case 3;
+         printf("pick a pasta");
+      case 4;
+          
+          printf("pick a french fries");
+      case 5;
+           printf("pick a sandwich");
+      default: printf("invaild choice");
+   }
+}
